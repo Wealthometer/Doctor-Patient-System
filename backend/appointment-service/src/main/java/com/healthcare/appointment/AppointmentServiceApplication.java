b2d4dfdf1a917a0e006c37064b4bcba63a977913
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 00:57:11 +0100

    Update AppointmentServiceApplication.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/AppointmentServiceApplication.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/AppointmentServiceApplication.java
@@ -10,3 +10,4 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class AppointmentServiceApplication {
     public static void main(String[] args) {
+        SpringApplication.run(AppointmentServiceApplication.class, args);
