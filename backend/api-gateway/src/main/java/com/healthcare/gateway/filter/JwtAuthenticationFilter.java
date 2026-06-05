dae2babc8c0479e1c26c8e03c20ba2735a3f71b7
Wealthometer <wealthhajoh87@gmail.com>
2026-06-05 23:32:16 +0100

    Update JwtAuthenticationFilter.java

--- backend/api-gateway/src/main/java/com/healthcare/gateway/filter/JwtAuthenticationFilter.java
+++ backend/api-gateway/src/main/java/com/healthcare/gateway/filter/JwtAuthenticationFilter.java
@@ -15,3 +15,4 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
