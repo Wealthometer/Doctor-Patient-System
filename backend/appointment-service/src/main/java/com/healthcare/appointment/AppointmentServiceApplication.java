f0d3c2c84dba2ecc14a65986b5e3513f5b20e19c
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 00:56:51 +0100

    Update AppointmentServiceApplication.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/AppointmentServiceApplication.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/AppointmentServiceApplication.java
@@ -3,3 +3,4 @@ package com.healthcare.appointment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
