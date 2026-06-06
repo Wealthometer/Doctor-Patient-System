9c15311450b4fd608b8cacce8e1cb945d370f740
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 00:53:58 +0100

    Update V1__init_notification_schema.sql

--- backend/notification-service/src/main/resources/db/migration/V1__init_notification_schema.sql
+++ backend/notification-service/src/main/resources/db/migration/V1__init_notification_schema.sql
@@ -6,3 +6,4 @@ CREATE TABLE notifications (
     id               UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     recipient_id     UUID,
     recipient_email  VARCHAR(255) NOT NULL,
+    recipient_phone  VARCHAR(20),
