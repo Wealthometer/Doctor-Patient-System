88f588e1bc2159be6f83e34cf9da33564f72b222
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:44:37 +0100

    Update PatientServiceClient.java

--- backend/billing-service/src/main/java/com/healthcare/billing/client/PatientServiceClient.java
+++ backend/billing-service/src/main/java/com/healthcare/billing/client/PatientServiceClient.java
@@ -3,3 +3,4 @@ package com.healthcare.billing.client;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
