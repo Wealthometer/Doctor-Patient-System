64628db5e2963b2164d5c567bb87ad98460342fc
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:20:10 +0100

    Update V1__init_auth_schema.sql

--- backend/auth-service/src/main/resources/db/migration/V1__init_auth_schema.sql
+++ backend/auth-service/src/main/resources/db/migration/V1__init_auth_schema.sql
@@ -9,3 +9,4 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL CHECK (role IN ('ADMIN','DOCTOR','PATIENT','NURSE')),
