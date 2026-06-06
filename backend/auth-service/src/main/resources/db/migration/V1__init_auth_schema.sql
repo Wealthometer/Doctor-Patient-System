ef709e94c4683376b2ec6a6d157833024ce82af6
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:20:04 +0100

    Update V1__init_auth_schema.sql

--- backend/auth-service/src/main/resources/db/migration/V1__init_auth_schema.sql
+++ backend/auth-service/src/main/resources/db/migration/V1__init_auth_schema.sql
@@ -5,3 +5,4 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
