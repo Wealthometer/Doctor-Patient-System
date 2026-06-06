fba99c8c018038562661f90e08ed34f99b177281
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 03:48:59 +0100

    Update AppointmentNotFoundException.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/exception/AppointmentNotFoundException.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/exception/AppointmentNotFoundException.java
@@ -2,3 +2,4 @@ package com.healthcare.appointment.exception;
 
 public class AppointmentNotFoundException extends RuntimeException {
     public AppointmentNotFoundException(String message) {
+        super(message);
