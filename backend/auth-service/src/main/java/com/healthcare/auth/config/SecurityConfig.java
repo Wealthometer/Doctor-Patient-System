7bfc336f032c9e7ae8593e76998bbffcc38c72f7
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 04:49:47 +0100

    Update SecurityConfig.java

--- backend/auth-service/src/main/java/com/healthcare/auth/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/config/SecurityConfig.java
@@ -7,3 +7,4 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
