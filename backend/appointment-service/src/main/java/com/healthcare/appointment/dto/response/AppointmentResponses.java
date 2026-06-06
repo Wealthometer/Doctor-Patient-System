cc5a38909ec95ca46350a3d88112c6e2c5d3ef95
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 03:35:13 +0100

    Update AppointmentResponses.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/dto/response/AppointmentResponses.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/dto/response/AppointmentResponses.java
@@ -2,3 +2,4 @@ package com.healthcare.appointment.dto.response;
 
 import com.healthcare.appointment.entity.AppointmentStatus;
 import com.healthcare.appointment.entity.AppointmentType;
+import lombok.Builder;
