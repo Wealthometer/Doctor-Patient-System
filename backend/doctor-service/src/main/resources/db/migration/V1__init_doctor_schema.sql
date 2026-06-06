d05ad38439497549665f9c6cafb96635ba3e67bb
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 21:21:02 +0100

    Update V1__init_doctor_schema.sql

--- backend/doctor-service/src/main/resources/db/migration/V1__init_doctor_schema.sql
+++ backend/doctor-service/src/main/resources/db/migration/V1__init_doctor_schema.sql
@@ -8,3 +8,4 @@ CREATE TABLE doctors (
     id                      UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id                 UUID NOT NULL UNIQUE,
     doctor_code             VARCHAR(20) NOT NULL UNIQUE,
+    first_name              VARCHAR(100) NOT NULL,
