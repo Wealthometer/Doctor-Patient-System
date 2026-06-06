369a4ddc97f6086cb3a1e7909278074d8f5bcccd
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:43:52 +0100

    Update BillingServiceApplication.java

--- backend/billing-service/src/main/java/com/healthcare/billing/BillingServiceApplication.java
+++ backend/billing-service/src/main/java/com/healthcare/billing/BillingServiceApplication.java
@@ -5,3 +5,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
