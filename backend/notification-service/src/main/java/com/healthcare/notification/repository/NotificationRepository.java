fabd8ba177da37e85ad56d6622b7d2f4383bea48
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 00:43:35 +0100

    Update NotificationRepository.java

--- backend/notification-service/src/main/java/com/healthcare/notification/repository/NotificationRepository.java
+++ backend/notification-service/src/main/java/com/healthcare/notification/repository/NotificationRepository.java
@@ -2,3 +2,4 @@ package com.healthcare.notification.repository;
 
 import com.healthcare.notification.entity.Notification;
 import com.healthcare.notification.entity.NotificationStatus;
+import org.springframework.data.domain.Page;
