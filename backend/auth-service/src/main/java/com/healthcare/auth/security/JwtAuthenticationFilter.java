873ec6de9a6f2417be0e55e9da936e6763feb8ef
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:51:05 +0100

    Update JwtAuthenticationFilter.java

--- backend/auth-service/src/main/java/com/healthcare/auth/security/JwtAuthenticationFilter.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/security/JwtAuthenticationFilter.java
@@ -9,3 +9,4 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
