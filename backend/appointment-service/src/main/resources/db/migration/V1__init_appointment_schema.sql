c53251fd96fa8714e952c40bc3b96ef708e2baa4
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 04:29:30 +0100

    Update V1__init_appointment_schema.sql

--- backend/appointment-service/src/main/resources/db/migration/V1__init_appointment_schema.sql
+++ backend/appointment-service/src/main/resources/db/migration/V1__init_appointment_schema.sql
@@ -23,5 +23,6 @@ CREATE TABLE appointments (
     confirmed_at         TIMESTAMP,
     completed_at         TIMESTAMP,
     created_at           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX idx_appt_patient_id    ON appointments(patient_id);
