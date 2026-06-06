9e472c08a15fa4a0c98200921871b8c8cb594e88
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 03:10:55 +0100

    Update AppointmentController.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/controller/AppointmentController.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/controller/AppointmentController.java
@@ -4,3 +4,4 @@ import com.healthcare.appointment.dto.request.AppointmentRequests.*;
 import com.healthcare.appointment.dto.response.AppointmentResponses.*;
 import com.healthcare.appointment.service.AppointmentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
