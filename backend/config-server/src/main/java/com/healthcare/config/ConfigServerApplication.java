40cce7348cd4cd2af104e8f5433deff74237a76b
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 13:41:02 +0100

    Update ConfigServerApplication.java

--- backend/config-server/src/main/java/com/healthcare/config/ConfigServerApplication.java
+++ backend/config-server/src/main/java/com/healthcare/config/ConfigServerApplication.java
@@ -11,3 +11,4 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
