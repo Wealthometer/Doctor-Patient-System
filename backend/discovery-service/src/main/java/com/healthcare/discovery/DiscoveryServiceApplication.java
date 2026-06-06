0b69a0ac10190f89461a35d52721db5068434623
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 14:01:08 +0100

    Update DiscoveryServiceApplication.java

--- backend/discovery-service/src/main/java/com/healthcare/discovery/DiscoveryServiceApplication.java
+++ backend/discovery-service/src/main/java/com/healthcare/discovery/DiscoveryServiceApplication.java
@@ -5,3 +5,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
