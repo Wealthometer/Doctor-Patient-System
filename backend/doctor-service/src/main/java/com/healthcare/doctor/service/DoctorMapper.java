7385ab634c1849517de03884aca318182924971b
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 20:38:03 +0100

    Update DoctorMapper.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorMapper.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorMapper.java
@@ -3,3 +3,4 @@ package com.healthcare.doctor.service;
 import com.healthcare.doctor.dto.request.DoctorRequests.*;
 import com.healthcare.doctor.dto.response.DoctorResponses.*;
 import com.healthcare.doctor.entity.Doctor;
+import org.mapstruct.*;
