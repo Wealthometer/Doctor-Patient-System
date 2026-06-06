5e191e03719f0f374c1092f90db2ca43442a57ce
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 00:43:04 +0100

    Update NotificationServiceApplication.java

--- backend/notification-service/src/main/java/com/healthcare/notification/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/healthcare/notification/NotificationServiceApplication.java
@@ -3,3 +3,4 @@ package com.healthcare.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
