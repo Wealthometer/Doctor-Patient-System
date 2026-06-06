bd0f6bbf58cf01b280d37034893cc6dd33843d7f
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 13:33:26 +0100

    Update V1__init_billing_schema.sql

--- backend/billing-service/src/main/resources/db/migration/V1__init_billing_schema.sql
+++ backend/billing-service/src/main/resources/db/migration/V1__init_billing_schema.sql
@@ -5,3 +5,4 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE invoices (
     id                   UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     invoice_number       VARCHAR(30) NOT NULL UNIQUE,
+    patient_id           UUID NOT NULL,
