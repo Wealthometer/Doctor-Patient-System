78e47cffce762c542de4949486d643ce0f9880d9
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 04:46:59 +0100

    Update AuthServiceApplication.java

--- backend/auth-service/src/main/java/com/healthcare/auth/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/AuthServiceApplication.java
@@ -2,3 +2,4 @@ package com.healthcare.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
