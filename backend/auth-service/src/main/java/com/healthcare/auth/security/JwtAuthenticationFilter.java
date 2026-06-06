0111510d323a401356c475440b874c38769f540b
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:51:13 +0100

    Update JwtAuthenticationFilter.java

--- backend/auth-service/src/main/java/com/healthcare/auth/security/JwtAuthenticationFilter.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/security/JwtAuthenticationFilter.java
@@ -14,3 +14,4 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
