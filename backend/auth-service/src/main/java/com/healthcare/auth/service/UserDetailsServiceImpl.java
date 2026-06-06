762cb0c20b3272fc232f54b352cd00a19942107f
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:15:09 +0100

    Update UserDetailsServiceImpl.java

--- backend/auth-service/src/main/java/com/healthcare/auth/service/UserDetailsServiceImpl.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/service/UserDetailsServiceImpl.java
@@ -4,3 +4,4 @@ import com.healthcare.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
