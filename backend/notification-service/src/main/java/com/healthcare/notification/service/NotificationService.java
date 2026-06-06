8f83c9d68ceacd5b451b5fbf5bf4ebda83401b81
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 00:46:55 +0100

    Update NotificationService.java

--- backend/notification-service/src/main/java/com/healthcare/notification/service/NotificationService.java
+++ backend/notification-service/src/main/java/com/healthcare/notification/service/NotificationService.java
@@ -52,6 +52,7 @@ public class NotificationService {
     private void sendEmail(Notification notification) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(notification.getRecipientEmail());
+        message.setSubject(notification.getSubject());
         message.setText(notification.getBody());
         mailSender.send(message);
     }
