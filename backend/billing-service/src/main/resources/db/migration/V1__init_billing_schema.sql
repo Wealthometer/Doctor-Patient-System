2c08f13feaac218b7bf2b79ab6eb71c71b508344
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 13:33:32 +0100

    Update V1__init_billing_schema.sql

--- backend/billing-service/src/main/resources/db/migration/V1__init_billing_schema.sql
+++ backend/billing-service/src/main/resources/db/migration/V1__init_billing_schema.sql
@@ -8,3 +8,4 @@ CREATE TABLE invoices (
     patient_id           UUID NOT NULL,
     patient_name         VARCHAR(200) NOT NULL,
     appointment_id       UUID,
+    appointment_number   VARCHAR(30),
