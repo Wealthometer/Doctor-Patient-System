4382b1f06e7a942d510502e1f854fa60f2829819
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:21:56 +0100

    Update AuthServiceTest.java

--- backend/auth-service/src/test/java/com/healthcare/auth/AuthServiceTest.java
+++ backend/auth-service/src/test/java/com/healthcare/auth/AuthServiceTest.java
@@ -2,3 +2,4 @@ package com.healthcare.auth;
 
 import com.healthcare.auth.dto.request.AuthRequests.*;
 import com.healthcare.auth.dto.response.AuthResponses.*;
+import com.healthcare.auth.entity.RefreshToken;
