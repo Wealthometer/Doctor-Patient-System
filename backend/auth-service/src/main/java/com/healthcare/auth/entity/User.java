04adffc87f615510cb12f2042088f3b0d6188b06
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:35:45 +0100

    Update User.java

--- backend/auth-service/src/main/java/com/healthcare/auth/entity/User.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/entity/User.java
@@ -6,3 +6,4 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
