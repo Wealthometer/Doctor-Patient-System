5b1f4b1a010f31f5c231a481662b9541aaf0a813
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:20:06 +0100

    Update V1__init_auth_schema.sql

--- backend/auth-service/src/main/resources/db/migration/V1__init_auth_schema.sql
+++ backend/auth-service/src/main/resources/db/migration/V1__init_auth_schema.sql
@@ -6,3 +6,4 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
