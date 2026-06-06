9905a3fef7a6301f03b2ca3b67103fdca7cdf1fc
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 21:21:12 +0100

    Update V1__init_doctor_schema.sql

--- backend/doctor-service/src/main/resources/db/migration/V1__init_doctor_schema.sql
+++ backend/doctor-service/src/main/resources/db/migration/V1__init_doctor_schema.sql
@@ -11,3 +11,4 @@ CREATE TABLE doctors (
     first_name              VARCHAR(100) NOT NULL,
     last_name               VARCHAR(100) NOT NULL,
     email                   VARCHAR(255) NOT NULL UNIQUE,
+    phone                   VARCHAR(20) NOT NULL,
