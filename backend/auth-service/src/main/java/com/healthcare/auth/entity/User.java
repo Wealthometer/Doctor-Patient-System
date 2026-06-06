990ec9cf562b3ead5ff3e59c0fb338e47249ecc0
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:39:33 +0100

    Update User.java

--- backend/auth-service/src/main/java/com/healthcare/auth/entity/User.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/entity/User.java
@@ -77,3 +77,4 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
