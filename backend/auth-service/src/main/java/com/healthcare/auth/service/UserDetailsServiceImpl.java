de6af200915b7057dd34c0d80fe2ea0d516d3329
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:15:07 +0100

    Update UserDetailsServiceImpl.java

--- backend/auth-service/src/main/java/com/healthcare/auth/service/UserDetailsServiceImpl.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/service/UserDetailsServiceImpl.java
@@ -3,3 +3,4 @@ package com.healthcare.auth.service;
 import com.healthcare.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
