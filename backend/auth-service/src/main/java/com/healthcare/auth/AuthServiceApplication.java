90f48cb97e62ea775a85f3c1135e8a8aad8eb8ef
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 04:47:14 +0100

    Update AuthServiceApplication.java

--- backend/auth-service/src/main/java/com/healthcare/auth/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/AuthServiceApplication.java
@@ -9,3 +9,4 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
