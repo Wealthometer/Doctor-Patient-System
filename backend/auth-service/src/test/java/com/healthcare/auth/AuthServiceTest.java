1a00261fb7d65ee4fa9a9df25a82eec4351459b7
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:24:23 +0100

    Update AuthServiceTest.java

--- backend/auth-service/src/test/java/com/healthcare/auth/AuthServiceTest.java
+++ backend/auth-service/src/test/java/com/healthcare/auth/AuthServiceTest.java
@@ -78,3 +78,4 @@ class AuthServiceTest {
         when(jwtService.generateToken(any())).thenReturn("accessToken");
         when(jwtService.generateRefreshToken(any())).thenReturn("refreshToken");
         when(jwtService.getJwtExpiration()).thenReturn(900000L);
+        when(refreshTokenRepository.save(any())).thenReturn(mock(RefreshToken.class));
