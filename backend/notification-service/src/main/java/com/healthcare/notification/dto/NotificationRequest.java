6c31793c5aff7068cfadf4638e364a0791a03909
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 00:21:33 +0100

    Update NotificationRequest.java

--- backend/notification-service/src/main/java/com/healthcare/notification/dto/NotificationRequest.java
+++ backend/notification-service/src/main/java/com/healthcare/notification/dto/NotificationRequest.java
@@ -3,3 +3,4 @@ package com.healthcare.notification.dto;
 import com.healthcare.notification.entity.NotificationChannel;
 import com.healthcare.notification.entity.NotificationType;
 import lombok.Builder;
+import lombok.Data;
