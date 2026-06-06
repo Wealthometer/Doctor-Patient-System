0c23ec679e39da986af1a9f0c1d7399c272bb327
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:48:09 +0100

    Update BillingController.java

--- backend/billing-service/src/main/java/com/healthcare/billing/controller/BillingController.java
+++ backend/billing-service/src/main/java/com/healthcare/billing/controller/BillingController.java
@@ -7,3 +7,4 @@ import com.healthcare.billing.service.BillingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
