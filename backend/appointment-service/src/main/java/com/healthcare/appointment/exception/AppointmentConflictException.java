11c561b8a97c349b0f7aae82637869f7f047f416
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 03:48:42 +0100

    Update AppointmentConflictException.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/exception/AppointmentConflictException.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/exception/AppointmentConflictException.java
@@ -3,3 +3,4 @@ package com.healthcare.appointment.exception;
 public class AppointmentConflictException extends RuntimeException {
     public AppointmentConflictException(String message) {
         super(message);
+    }
