f7e8bafceac18263ef0dd32bb23e2fae37741b0c
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:26:51 +0100

    Update AuthRequests.java

--- backend/auth-service/src/main/java/com/healthcare/auth/dto/request/AuthRequests.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/dto/request/AuthRequests.java
@@ -22,3 +22,4 @@ public class AuthRequests {
         @NotBlank(message = "Password is required")
         @Size(min = 8, message = "Password must be at least 8 characters")
         private String password;
+
