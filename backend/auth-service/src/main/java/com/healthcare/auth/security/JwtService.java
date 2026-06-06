7835d0bf0b7cf75a1b27d5d2a97f53a6771a7075
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:54:11 +0100

    Update JwtService.java

--- backend/auth-service/src/main/java/com/healthcare/auth/security/JwtService.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/security/JwtService.java
@@ -2,4 +2,5 @@ package com.healthcare.auth.security;
 
 import com.healthcare.auth.entity.User;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
