1b757a570ae3700afc8592c8c6a0269535a5a598
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 20:39:10 +0100

    Update DoctorMapper.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorMapper.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorMapper.java
@@ -25,3 +25,4 @@ public interface DoctorMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "userId", ignore = true)
