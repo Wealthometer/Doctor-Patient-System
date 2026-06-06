92c71d82ee0ed253099b78b52434f179197c144e
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 19:04:46 +0100

    Update DoctorController.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/controller/DoctorController.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/controller/DoctorController.java
@@ -6,3 +6,4 @@ import com.healthcare.doctor.entity.DoctorStatus;
 import com.healthcare.doctor.service.DoctorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
