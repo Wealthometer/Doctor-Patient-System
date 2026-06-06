84da07b68f7afa8bd2b1686dee16928c1e3879fa
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 06:15:20 +0100

    Update UserDetailsServiceImpl.java

--- backend/auth-service/src/main/java/com/healthcare/auth/service/UserDetailsServiceImpl.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/service/UserDetailsServiceImpl.java
@@ -8,3 +8,4 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
