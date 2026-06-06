0b8301d7748a6ea793e481583d41ee3280e03f2f
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 03:11:14 +0100

    Update AppointmentController.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/controller/AppointmentController.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/controller/AppointmentController.java
@@ -11,4 +11,5 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
