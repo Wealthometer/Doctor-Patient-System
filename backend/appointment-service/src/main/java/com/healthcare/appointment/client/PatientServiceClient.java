eefb14247afec9ec29f84662c42c5e2b9ae510bd
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 03:09:41 +0100

    Update PatientServiceClient.java

--- backend/appointment-service/src/main/java/com/healthcare/appointment/client/PatientServiceClient.java
+++ backend/appointment-service/src/main/java/com/healthcare/appointment/client/PatientServiceClient.java
@@ -6,3 +6,4 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
