116f0ba4155357b9db63727c115ee3a42c7ee8c5
Wealthometer <wealthhajoh87@gmail.com>
2026-06-06 05:51:48 +0100

    Update JwtAuthenticationFilter.java

--- backend/auth-service/src/main/java/com/healthcare/auth/security/JwtAuthenticationFilter.java
+++ backend/auth-service/src/main/java/com/healthcare/auth/security/JwtAuthenticationFilter.java
@@ -32,3 +32,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
 
+        final String authHeader = request.getHeader("Authorization");
