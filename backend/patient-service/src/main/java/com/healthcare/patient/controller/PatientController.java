f3c9b716b34a49e845cbb0a3778a6603fcca204a
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 03:33:39 +0100

    Update PatientController.java

--- backend/patient-service/src/main/java/com/healthcare/patient/controller/PatientController.java
+++ backend/patient-service/src/main/java/com/healthcare/patient/controller/PatientController.java
@@ -7,3 +7,4 @@ import com.healthcare.patient.service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
