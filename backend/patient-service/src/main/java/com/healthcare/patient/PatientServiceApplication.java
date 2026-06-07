9112333a2e01b4fd7e6ef390908e1d227411fc1a
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 15:48:23 +0100

    Update PatientServiceApplication.java

--- backend/patient-service/src/main/java/com/healthcare/patient/PatientServiceApplication.java
+++ backend/patient-service/src/main/java/com/healthcare/patient/PatientServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+public class PatientServiceApplication {
         SpringApplication.run(PatientServiceApplication.class, args);
     }
 }
