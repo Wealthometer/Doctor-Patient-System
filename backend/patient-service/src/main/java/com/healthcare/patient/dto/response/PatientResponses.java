557a73e42f04db841844088df6c01d14d1dc69d4
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 07:33:18 +0100

    Update PatientResponses.java

--- backend/patient-service/src/main/java/com/healthcare/patient/dto/response/PatientResponses.java
+++ backend/patient-service/src/main/java/com/healthcare/patient/dto/response/PatientResponses.java
@@ -3,3 +3,4 @@ package com.healthcare.patient.dto.response;
 import com.healthcare.patient.entity.Gender;
 import com.healthcare.patient.entity.PatientStatus;
 import lombok.Builder;
+import lombok.Data;
