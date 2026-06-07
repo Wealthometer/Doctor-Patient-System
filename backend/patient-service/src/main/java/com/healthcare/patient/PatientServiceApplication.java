25cb3003860fd6b3d2a4b3c021c5723ed5e9f51e
Wealthometer <wealthhajoh87@gmail.com>
2026-06-07 15:48:14 +0100

    Update PatientServiceApplication.java

--- backend/patient-service/src/main/java/com/healthcare/patient/PatientServiceApplication.java
+++ backend/patient-service/src/main/java/com/healthcare/patient/PatientServiceApplication.java
@@ -2,6 +2,7 @@ package com.healthcare.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
         SpringApplication.run(PatientServiceApplication.class, args);
     }
 }
