94135b1224cffb8b152c7e48778c3e4b738179fc
Wealthometer <wealthhajoh87@gmail.com>
2026-06-08 13:06:42 +0100

    Update PrescriptionServiceApplication.java

--- backend/prescription-service/src/main/java/com/healthcare/prescription/PrescriptionServiceApplication.java
+++ backend/prescription-service/src/main/java/com/healthcare/prescription/PrescriptionServiceApplication.java
@@ -5,4 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
