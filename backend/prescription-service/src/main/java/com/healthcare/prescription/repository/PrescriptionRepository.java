109a953a0a7e5668f6252006d8743d8366e3ea3d
Wealthometer <wealthhajoh87@gmail.com>
2026-06-08 13:09:30 +0100

    Update PrescriptionRepository.java

--- backend/prescription-service/src/main/java/com/healthcare/prescription/repository/PrescriptionRepository.java
+++ backend/prescription-service/src/main/java/com/healthcare/prescription/repository/PrescriptionRepository.java
@@ -2,3 +2,4 @@ package com.healthcare.prescription.repository;
 
 import com.healthcare.prescription.entity.Prescription;
 import com.healthcare.prescription.entity.PrescriptionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
