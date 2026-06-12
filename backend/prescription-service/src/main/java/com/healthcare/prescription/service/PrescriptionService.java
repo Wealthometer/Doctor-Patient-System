3871d55bc2fac8e84882e650845d766601f727c0
Wealthometer <wealthhajoh87@gmail.com>
2026-06-12 17:14:50 +0100

    Update PrescriptionService.java

--- backend/prescription-service/src/main/java/com/healthcare/prescription/service/PrescriptionService.java
+++ backend/prescription-service/src/main/java/com/healthcare/prescription/service/PrescriptionService.java
@@ -2,4 +2,5 @@ package com.healthcare.prescription.service;
 
 import com.healthcare.prescription.dto.request.PrescriptionRequests.*;
 import com.healthcare.prescription.entity.Prescription;
+import com.healthcare.prescription.entity.PrescriptionItem;
 import com.healthcare.prescription.entity.PrescriptionStatus;
