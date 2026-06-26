350fb98d032605e287cf1a9f1666dee103b11545
Wealthometer <wealthhajoh87@gmail.com>
2026-06-26 19:37:27 +0100

    Update LoggingFilter.java

--- backend/api-gateway/src/main/java/com/healthcare/gateway/filter/LoggingFilter.java
+++ backend/api-gateway/src/main/java/com/healthcare/gateway/filter/LoggingFilter.java
@@ -1,6 +1,7 @@
 package com.healthcare.gateway.filter;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
