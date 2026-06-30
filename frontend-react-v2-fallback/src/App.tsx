13c0ac641a204b8401d01447556137ced7e544e4
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 19:53:30 +0100

    Update App.tsx

--- frontend-react-v2-fallback/src/App.tsx
+++ frontend-react-v2-fallback/src/App.tsx
@@ -26,3 +26,4 @@ export default function App() {
             style: { borderRadius: '10px', fontSize: '13px', fontWeight: 500 },
             success: { iconTheme: { primary: '#10b981', secondary: '#fff' } },
             error: { iconTheme: { primary: '#ef4444', secondary: '#fff' } },
+          }}
