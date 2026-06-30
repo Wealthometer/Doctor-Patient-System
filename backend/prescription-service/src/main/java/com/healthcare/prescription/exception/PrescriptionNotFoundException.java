aeab2b550ce5c14dcaf947ef7ef884e8e6d36583
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 15:15:42 +0100

    Update PrescriptionNotFoundException.java

--- backend/prescription-service/src/main/java/com/healthcare/prescription/exception/PrescriptionNotFoundException.java
+++ backend/prescription-service/src/main/java/com/healthcare/prescription/exception/PrescriptionNotFoundException.java
@@ -3,3 +3,4 @@ package com.healthcare.prescription.exception;
 public class PrescriptionNotFoundException extends RuntimeException {
     public PrescriptionNotFoundException(String message) {
         super(message);
+    }
