897af0c4347274be7aa7065f2f4f7208d93a41c7
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 14:55:32 +0100

    Update DoctorService.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorService.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorService.java
@@ -4,6 +4,7 @@ import com.healthcare.doctor.dto.request.DoctorRequests.CreateDoctorRequest;
 import com.healthcare.doctor.dto.request.DoctorRequests.UpdateDoctorRequest;
 import com.healthcare.doctor.dto.request.DoctorRequests.RatingRequest;
 import com.healthcare.doctor.dto.response.DoctorResponses.DoctorResponse;
+import com.healthcare.doctor.dto.response.DoctorResponses.DoctorSummaryResponse;
 import com.healthcare.doctor.entity.Doctor;
 import com.healthcare.doctor.entity.DoctorStatus;
 import com.healthcare.doctor.exception.DoctorAlreadyExistsException;
