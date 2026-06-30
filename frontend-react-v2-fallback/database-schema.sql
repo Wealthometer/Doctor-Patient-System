4a0f6b2ccc021e46100c1ed7ded888e85f65d300
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 18:47:20 +0100

    Update database-schema.sql

--- frontend-react-v2-fallback/database-schema.sql
+++ frontend-react-v2-fallback/database-schema.sql
@@ -132,4 +132,5 @@ CREATE TABLE doctors (
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
