a4084e5e2bd3841ca311eb96d3b0754c22f79d50
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 15:18:25 +0100

    Update database-schema.sql

--- frontend-react-v2-fallback/database-schema.sql
+++ frontend-react-v2-fallback/database-schema.sql
@@ -28,3 +28,4 @@ CREATE TABLE users (
 
 CREATE TABLE refresh_tokens (
     id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id     UUID        NOT NULL REFERENCES users(id) ON DELETE CASCADE,
