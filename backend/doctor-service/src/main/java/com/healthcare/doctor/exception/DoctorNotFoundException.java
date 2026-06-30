8507044b9254dee5626349eaea179605c885c660
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 14:55:00 +0100

    Update DoctorNotFoundException.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/exception/DoctorNotFoundException.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/exception/DoctorNotFoundException.java
@@ -2,5 +2,6 @@ package com.healthcare.doctor.exception;
 public class DoctorNotFoundException extends RuntimeException {
         super(message);
 
+    public DoctorNotFoundException(String message, Throwable cause) {
         super(message, cause);
 }
