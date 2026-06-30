ec22288db4dd7baa95963f80ce8980516b7068a1
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 16:42:52 +0100

    Update vite.config.ts

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8080',
         changeOrigin: true,
       },
     },
