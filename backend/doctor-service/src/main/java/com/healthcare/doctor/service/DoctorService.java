b80322c541c8c0a3e06acc72ac14c81bcb77febb
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 14:55:16 +0100

    Update DoctorService.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorService.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorService.java
@@ -1,6 +1,5 @@
 package com.healthcare.doctor.service;
 
-import com.healthcare.doctor.dto.request.DoctorRequests.*;
 import com.healthcare.doctor.dto.response.DoctorResponses.*;
 import com.healthcare.doctor.entity.Doctor;
 import com.healthcare.doctor.entity.DoctorStatus;
