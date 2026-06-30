1666a6eaaf933e9066bb8642d516ec58f770e17a
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 16:53:11 +0100

    Update index.ts

--- frontend-react/src/types/index.ts
+++ frontend-react/src/types/index.ts
@@ -66,7 +66,6 @@ export interface PatientResponse {
   updatedAt: string;
 }
 
-export type DoctorStatus = 'ACTIVE' | 'INACTIVE' | 'ON_LEAVE' | 'SUSPENDED';
 
 export interface Doctor {
   id: string;
