b7bb1a69ead69e72366bf389114605d91dc43feb
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 17:01:23 +0100

    Update index.ts

--- frontend-react/src/types/index.ts
+++ frontend-react/src/types/index.ts
@@ -251,7 +251,6 @@ export interface Prescription {
 
 export interface CreatePrescriptionRequest {
   patientId: string;
-  taxAmount: number;
   totalAmount: number;
   paidAmount: number;
   balanceDue: number;
