b4bd0557fe78716158795422c6d55918a266fa1b
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 19:57:59 +0100

    Update App.tsx

--- frontend-react-v2-fallback/src/App.tsx
+++ frontend-react-v2-fallback/src/App.tsx
@@ -57,3 +57,4 @@ export default function App() {
 
             <Route path="appointments" element={<AppointmentsPage />} />
             <Route path="prescriptions" element={<PrescriptionsPage />} />
+
