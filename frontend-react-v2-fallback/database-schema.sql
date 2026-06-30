344f3d52f167e701eef3de3cddd6c4a7d8529666
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 15:18:05 +0100

    Update database-schema.sql

--- frontend-react-v2-fallback/database-schema.sql
+++ frontend-react-v2-fallback/database-schema.sql
@@ -13,4 +13,5 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
     id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username        VARCHAR(50)  UNIQUE NOT NULL,
+    email           VARCHAR(255) UNIQUE NOT NULL,
 
