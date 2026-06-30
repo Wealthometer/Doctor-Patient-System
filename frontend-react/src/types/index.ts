3d8ddb1fabb9e3f3664dccfb4722cbeb6d55fb7d
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 16:53:26 +0100

    Update index.ts

--- frontend-react/src/types/index.ts
+++ frontend-react/src/types/index.ts
@@ -69,6 +69,7 @@ export interface PatientResponse {
 export interface CreatePatientRequest {
   userId: string;
   firstName: string;
+  lastName: string;
 
 export interface Doctor {
   id: string;
