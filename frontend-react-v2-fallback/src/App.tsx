781fd6a961ba8b7f54c402675c4444f82f14281b
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 18:53:10 +0100

    Update App.tsx

--- frontend-react-v2-fallback/src/App.tsx
+++ frontend-react-v2-fallback/src/App.tsx
@@ -2,4 +2,5 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
+import { store } from './store';
 import { AppLayout } from './components/layout/AppLayout';
