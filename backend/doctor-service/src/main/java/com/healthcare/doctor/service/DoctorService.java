efe0a95ef3de65fb9d4b54c51c1e69dda6417c7f
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 14:55:18 +0100

    Update DoctorService.java

--- backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorService.java
+++ backend/doctor-service/src/main/java/com/healthcare/doctor/service/DoctorService.java
@@ -1,6 +1,5 @@
 package com.healthcare.doctor.service;
 
-import com.healthcare.doctor.dto.response.DoctorResponses.*;
 import com.healthcare.doctor.entity.Doctor;
 import com.healthcare.doctor.entity.DoctorStatus;
 import com.healthcare.doctor.exception.DoctorAlreadyExistsException;
