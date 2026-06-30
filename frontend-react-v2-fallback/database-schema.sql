d2f514b9811391e5ef6f03ab4399acd9d6ad5fd1
Wealthometer <wealthhajoh87@gmail.com>
2026-06-30 15:18:08 +0100

    Update database-schema.sql

--- frontend-react-v2-fallback/database-schema.sql
+++ frontend-react-v2-fallback/database-schema.sql
@@ -15,4 +15,5 @@ CREATE TABLE users (
     username        VARCHAR(50)  UNIQUE NOT NULL,
     email           VARCHAR(255) UNIQUE NOT NULL,
     password        VARCHAR(255) NOT NULL,
+    first_name      VARCHAR(100) NOT NULL,
 
