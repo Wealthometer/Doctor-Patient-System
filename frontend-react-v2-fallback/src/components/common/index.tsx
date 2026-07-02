e72a19fecdf7eeb1a9a98b46a54473512204c76a
Wealthometer <wealthhajoh87@gmail.com>
2026-07-02 15:31:05 +0100

    Update index.tsx

--- frontend-react-v2-fallback/src/components/common/index.tsx
+++ frontend-react-v2-fallback/src/components/common/index.tsx
@@ -126,3 +126,4 @@ export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 );
 Textarea.displayName = 'Textarea';
 
+// ── Card ──────────────────────────────────────────────────────────────────────
