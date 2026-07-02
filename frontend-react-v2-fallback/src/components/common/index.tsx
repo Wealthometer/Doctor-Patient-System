e98b75d7ffae290c2eb1f1fe7bdd651bcade8290
Wealthometer <wealthhajoh87@gmail.com>
2026-07-02 15:30:54 +0100

    Update index.tsx

--- frontend-react-v2-fallback/src/components/common/index.tsx
+++ frontend-react-v2-fallback/src/components/common/index.tsx
@@ -121,3 +121,4 @@ export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         {...props}
       />
       {error && <p className="text-xs text-red-600">{error}</p>}
+    </div>
