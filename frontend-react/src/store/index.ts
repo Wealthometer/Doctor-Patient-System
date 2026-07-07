2fdd347dd6544306c3b7ffc5f9694264969c1259
Agbaje Stephen <agbajestephen5@gmail.com>
2026-07-07 22:17:46 +0100

    Update index.ts

--- frontend-react/src/store/index.ts
+++ frontend-react/src/store/index.ts
@@ -6,6 +6,7 @@ import notificationReducer from './slices/notificationSlice';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    notifications: notificationReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
